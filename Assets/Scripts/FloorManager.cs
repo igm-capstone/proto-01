cfca74845faab827e85e54ec962a1586a52d860e
Karan Sahu <karansahu1991@gmail.com>
2015-09-06 17:56:49 -0400

    Updated ghost tag. Switches are working. The wrong traps are glowing and disappearing

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -6,7 +6,7 @@ public class FloorManager : MonoBehaviour
     [SerializeField]
     Transform[] floors = new Transform[4];
     [SerializeField]
-    private float trapDelay;
+    private float flashTime;
     [SerializeField]
     private Material floorFlashMaterial;
     [SerializeField]
@@ -14,29 +14,14 @@ public class FloorManager : MonoBehaviour
     private Color origFlashColor;
     private Material floorBaseMaterial;
     private bool trapBool = false;
+    private int switchNum;
 
     void Start()
     {
         floorBaseMaterial = floors[1].GetChild(1).GetComponent<Renderer>().material;
         origFlashColor = floorFlashMaterial.color;
-        StartCoroutine("activateFlash", trapDelay);
     }
 
-    IEnumerator activateFlash(float trapDelay)
-    {        
-        yield return new WaitForSeconds(trapDelay);
-        foreach (Transform floor in floors[0])
-        {
-            floor.gameObject.GetComponent<Renderer>().material = floorFlashMaterial;
-        }
-        trapBool = true;
-        
-        yield return new WaitForSeconds(trapDelay);
-        trapBool = false;
-        floorFlashMaterial.color = floorBaseMaterial.color;
-        
-        StartCoroutine("activateTrap", trapDelay);
-    }
 
     void Update()
     {
@@ -47,23 +32,60 @@ public class FloorManager : MonoBehaviour
         }
     }
 
-    IEnumerator activateTrap(float trapDelay)
+    IEnumerator activateFlash(float flashTime)
+    {        
+        //yield return new WaitForSeconds(flashTime);
+        foreach (Transform floor in floors[switchNum])
+        {
+            floor.gameObject.GetComponent<Renderer>().material = floorFlashMaterial;
+        }
+        trapBool = true;
+        
+        yield return new WaitForSeconds(flashTime);
+        trapBool = false;
+        floorFlashMaterial.color = floorBaseMaterial.color;
+        
+        StartCoroutine("activateTrap", flashTime);
+    }
+
+    IEnumerator activateTrap(float flashTime)
     {
         //turn off the respective floors
-        foreach(Transform floor in floors[0])
+        foreach(Transform floor in floors[switchNum])
         {
             floor.gameObject.SetActive(false);
         }
 
         //wait for some time
-        yield return new WaitForSeconds(trapDelay);
-        foreach (Transform floor in floors[0])
+        yield return new WaitForSeconds(flashTime);
+        foreach (Transform floor in floors[switchNum])
         {
             floor.gameObject.SetActive(true);
-           // floor.gameObject.GetComponent<Renderer>().material = floorBaseMaterial;
         }
-       
-        //start the flash coroutine
-        StartCoroutine("activateFlash", trapDelay);
+    }
+
+    public void trapControl(string switchName)
+    {
+        switch (switchName)
+        {
+            case "Top":
+                switchNum = 0;
+                StartCoroutine("activateFlash", flashTime);
+                break;
+            case "Left":
+                switchNum = 1;
+                StartCoroutine("activateFlash", flashTime);
+                break;
+            case "Bottom":
+                switchNum = 2;
+                StartCoroutine("activateFlash", flashTime);
+                break;
+            case "Right":
+                switchNum = 3;
+                StartCoroutine("activateFlash", flashTime);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/SwitchControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchControl : MonoBehaviour 
+{
+    FloorManager floorManObj;
+
+    void Start()
+    {
+        floorManObj = GameObject.Find("FloorBase").GetComponent<FloorManager>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("hit");
+
+        if (other.gameObject.tag == "Ghost")
+        {
+            floorManObj.trapControl(gameObject.name);                        
+        }
+    }
+}
