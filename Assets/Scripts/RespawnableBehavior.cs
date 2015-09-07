7f2b21d8b6583947bc2f17e4643fb59fd3219c71
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-09-06 20:44:46 -0400

    Added Respawn to player and pillars

---
+++ Assets/Scripts/RespawnableBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnableBehavior : MonoBehaviour {
+
+    private Vector3 mRespawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        mRespawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -10.0)
+        {
+            transform.position = mRespawnPoint;
+        }
+	}
+}
