8f0c669cdef04a2b80c5ba465d988bb0b7cfcc27
Henrique Chaltein <hxs1151@rit.edu>
2015-09-06 23:38:19 -0400

    Greatly Revamped Blocks and Ladders level. Removed Excess Sphere Colliders form Player Prefab (Player 2 had 3 Spheres Colliders).

--- Assets/Scripts/RespawnableBehavior.cs
+++ Assets/Scripts/RespawnableBehavior.cs
@@ -8,6 +8,7 @@ public class RespawnableBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         mRespawnPoint = transform.position;
+        Debug.Log(this.name + ": " + mRespawnPoint.ToString());
 	}
 	
 	// Update is called once per frame
