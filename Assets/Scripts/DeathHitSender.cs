531aa9928c1327565c6fcbaa2a5326d667013897
Lucas Vasconcelos <garrafote@gmail.com>
2015-09-06 21:45:22 -0400

    die message being properly handled

--- Assets/Scripts/DeathHitSender.cs
+++ Assets/Scripts/DeathHitSender.cs
@@ -6,8 +6,9 @@ public class DeathHitSender : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if ((other.gameObject.layer == LayerMask.NameToLayer("Player")) 
+        if (((other.gameObject.layer == LayerMask.NameToLayer("Player")) 
             || (other.gameObject.layer == LayerMask.NameToLayer("Ghost")))
+            && other.name == "UpperCollider")
         {
             if (other.transform.parent == null)
             {
