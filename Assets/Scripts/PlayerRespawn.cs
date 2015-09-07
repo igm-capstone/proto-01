f399b14cb9b409fc2e781aa2447339bc1e0e4f73
Henrique Chaltein <hxs1151@rit.edu>
2015-09-06 20:05:58 -0400

    Finished Player Respawn. Finished "Blocks and Ladders" level.

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class PlayerRespawn : MonoBehaviour {
 
-    void onTriggerEnter(Collider Other)
+    void OnTriggerEnter(Collider Other)
     {
-
+        if (Other.tag == "Player")
+        {
+            Other.transform.position = Other.GetComponent<PlayerController>().PlyrStartPos;
+        }
     }
 }
